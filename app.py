ef03f7fed1ec8ca5e5a7feca6d0a8853ee262b26
Joshua Weg <Joshua.weg.95@gmail.com>
2024-10-12 15:11:54 -0700

    added project index

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ from twelvelabs.models.task import Task
 app = Flask(__name__)
 
 # Replace with your actual Twelve Labs API key
-TWELVE_LABS_API_KEY = "tlk_241Z16H2R70KP22J4CV3K22801XG"
-TWELVE_LABS_API_URL = "https://api.twelvelabs.io/v1.1/search"
-INDEX_ID = "66f1cde8163dbc55ba3bb220"
+TWELVE_LABS_API_KEY = ""
+INDEX_ID = "670add2fe5620307b898c1b0"
 
 client = TwelveLabs(api_key=TWELVE_LABS_API_KEY)
 def print_page(page):
     for clip in page:
         print(f"video_id={clip.video_id} score={clip.score}")
+        #print(clip)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -23,14 +23,21 @@ def index():
     return render_template('index.html')
 
 def search_videos(query):
-    response = client.search.query(INDEX_ID,query_text=query,options=['visual'])
+    response = client.search.query(INDEX_ID, query_text=query, options=['visual'])
+    results = []
     print_page(response.data)
+    results.extend(response.data)
+    
     while True:
         try:
-            print_page(next(response))
+            next_page = next(response)
+            print_page(next_page)
+            results.extend(next_page)
         except StopIteration:
             break
-    return response.data
+    
+    return results
+
 
 if __name__ == '__main__':
     app.run(debug=True)
